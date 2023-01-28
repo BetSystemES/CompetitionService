7935ef6634338a661a40a34b2ef93cbaaaee2f1d
bynikk <bynik2001@gmail.com>
2023-01-28 23:39:03 +0300

    implement service mapping profile

---
+++ src/CompetitionService.Grpc/Infastructure/Mappings/CompetitionServiceProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+
+using BusinessModels = CompetitionService.BusinessLogic.Models;
+
+namespace CompetitionService.Grpc.Infastructure.Mappings
+{
+    public class CompetitionServiceProfile : Profile
+    {
+        public CompetitionServiceProfile()
+        {
+            CreateMap<string, Guid>()
+                .ConvertUsing((x, res) => res = Guid.TryParse(x, out var id) ? id : Guid.Empty);
+            CreateMap<Guid?, string>()
+                .ConvertUsing((x, res) => res = x?.ToString() ?? string.Empty);
+
+            CreateMap<DateTime, Timestamp>()
+                .ConvertUsing(x => Timestamp.FromDateTime(x));
+            CreateMap<Timestamp, DateTime>()
+                .ConvertUsing(x => x.ToDateTime());
+
+            CreateMap<BusinessModels.Coefficient, Coefficient>()
+                .ReverseMap();
+            CreateMap<BusinessModels.CoefficientGroup, CoefficientGroup>()
+                .ReverseMap();
+            CreateMap<BusinessModels.Competitions.CompetitionBase, CompetitionBase>()
+                .ReverseMap();
+            CreateMap<BusinessModels.Competitions.CompetitionDota2, CompetitionDota2>()
+                .ReverseMap();
+        }
+    }
+}
