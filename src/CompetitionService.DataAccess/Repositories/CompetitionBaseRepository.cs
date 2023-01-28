d65600f5ec1bdd35032cafa32e5dbae6854da7e3
bynikk <bynik2001@gmail.com>
2023-01-28 23:34:58 +0300

    implement repositores

---
+++ src/CompetitionService.DataAccess/Repositories/CompetitionBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using CompetitionService.BusinessLogic.Contracts.DataAccess.Repositories;
+using CompetitionService.BusinessLogic.Models.Competitions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompetitionService.DataAccess.Repositories
+{
+    public class CompetitionBaseRepository : ICompetitionBaseRepository
+    {
+        private readonly DbSet<CompetitionBase> _entities;
+
+        public CompetitionBaseRepository(DbSet<CompetitionBase> entities)
+        {
+            _entities = entities;
+        }
+
+        public Task Create(CompetitionBase item, CancellationToken token)
+        {
+            _entities.Add(item);
+
+            return Task.CompletedTask;
+        }
+
+        public Task Update(CompetitionBase item, CancellationToken token)
+        {
+            _entities.Update(item);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/CompetitionService.DataAccess/Repositories/CompetitionDota2Repository.cs
@@ -0,0 +1,30 @@
+﻿using CompetitionService.BusinessLogic.Contracts.DataAccess.Repositories;
+using CompetitionService.BusinessLogic.Models.Competitions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompetitionService.DataAccess.Repositories
+{
+    public class CompetitionDota2Repository : ICompetitionRepository<CompetitionDota2>
+    {
+        private readonly DbSet<CompetitionDota2> _entities;
+
+        public CompetitionDota2Repository(DbSet<CompetitionDota2> entities)
+        {
+            _entities = entities;
+        }
+
+        public Task Create(CompetitionDota2 item, CancellationToken token)
+        {
+            _entities.Add(item);
+
+            return Task.CompletedTask;
+        }
+
+        public Task Update(CompetitionDota2 item, CancellationToken token)
+        {
+            _entities.Update(item);
+
+            return Task.CompletedTask;
+        }
+    }
+}
