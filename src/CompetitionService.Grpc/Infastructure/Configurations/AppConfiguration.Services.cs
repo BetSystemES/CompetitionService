0156ea46df8a65c649a0fdf0f7696718430f8dc5
bynikk <bynik2001@gmail.com>
2023-01-28 23:35:40 +0300

    update DI extensions

---
+++ src/CompetitionService.Grpc/Infastructure/Configurations/AppConfiguration.Services.cs
@@ -0,0 +1,23 @@
+﻿using CompetitionService.BusinessLogic.Contracts.Services;
+using CompetitionService.BusinessLogic.Services;
+
+using BusinessModels = CompetitionService.BusinessLogic.Models;
+
+namespace CompetitionService.Grpc.Infastructure.Configurations
+{
+    public static partial class AppConfiguration
+    {
+        /// <summary>
+        /// Adds the infrastructure services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <returns>IServiceCollection.</returns>
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(typeof(AuctionProfile).Assembly);
+            services.AddScoped<ICompetitionService<BusinessModels.Competitions.CompetitionDota2>, CompetitionDota2Service>();
+
+            return services;
+        }
+    }
+}
--- src/CompetitionService.Grpc/Program.cs
+++ src/CompetitionService.Grpc/Program.cs
@@ -1,17 +1,30 @@
-using CompetitionService.Grpc.Services;
+﻿using CompetitionService.DataAccess;
+using CompetitionService.Grpc.Infastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+var configuration = builder.Configuration;
 
-// Add services to the container.
-builder.Services.AddGrpc();
+builder.Services
+    .AddGrpc()
+    .Services
+    .AddInfrastructureServices()
+    .AddProviders()
+    .AddRepositories()
+    .AddPostgreSqlContext(options =>
+    {
+        options.UseNpgsql(configuration.GetConnectionString("CompetitionDb"));
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<CompetitionService.Grpc.Services.CompetitionService>();
 
 app.Run();
+
+
+namespace CompetitionService.Grpc
+{
+    public partial class Program { }
+}
