3c2b26e158067182407d15931e7a3a39bd8901e2
bynikk <bynik2001@gmail.com>
2023-01-29 00:00:53 +0300

    add automapper test

---
+++ tests/CompetitionService.IntegrationTests/Infastructure/AutomapperMappingTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompetitionService.IntegrationTests.Infastructure
+{
+    public class AutomapperMappingTests : IClassFixture<GrpcAppFactory>, IDisposable
+    {
+        private readonly IServiceScope _scope;
+        private readonly IMapper _mapper;
+
+        public AutomapperMappingTests(GrpcAppFactory factory)
+        {
+            _scope = factory.Services.CreateScope();
+            _mapper = _scope.ServiceProvider.GetRequiredService<IMapper>();
+        }
+
+        [Fact]
+        public void AutoMapper_Should_Have_Valid_Configuration()
+        {
+            _mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+    }
+}
