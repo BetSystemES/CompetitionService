655e2ed3bc6a0e8be5730e5a19e810347548493a
bynikk <bynik2001@gmail.com>
2023-01-30 14:49:39 +0300

    implement coefficient service

---
+++ src/CompetitionService.BusinessLogic/Contracts/Services/ICoefficientService.cs
@@ -0,0 +1,7 @@
+﻿namespace CompetitionService.BusinessLogic.Contracts.Services
+{
+    public interface ICoefficientService
+    {
+        Task DepositToCoefficientById(Guid id, double amount, CancellationToken token);
+    }
+}
---
+++ src/CompetitionService.BusinessLogic/Services/ICoefficientService.cs
@@ -0,0 +1,33 @@
+﻿using CompetitionService.BusinessLogic.Contracts.DataAccess;
+using CompetitionService.BusinessLogic.Contracts.DataAccess.Providers;
+using CompetitionService.BusinessLogic.Contracts.DataAccess.Repositories;
+using CompetitionService.BusinessLogic.Contracts.Services;
+
+namespace CompetitionService.BusinessLogic.Services
+{
+    public class CoefficientService : ICoefficientService
+    {
+        private readonly ICoefficientProvider _provider;
+        private readonly ICoefficientRepository _repository;
+        private readonly IDataContext _context;
+
+        public CoefficientService(
+            ICoefficientProvider provider,
+            ICoefficientRepository repository,
+            IDataContext context)
+        {
+            _provider = provider;
+            _repository = repository;
+            _context = context;
+        }
+        public async Task DepositToCoefficientById(Guid id, double amount, CancellationToken token)
+        {
+            var coefficient = await _provider.GetById(id, token);
+
+            coefficient.Amount += amount;
+
+            await _repository.Update(coefficient, token);
+            await _context.SaveChanges(token);
+        }
+    }
+}
