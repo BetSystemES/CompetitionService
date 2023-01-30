af731e3a9df4e5d1981a1882114497ba16965f72
bynikk <bynik2001@gmail.com>
2023-01-30 14:49:55 +0300

    implement competitionBase service

---
+++ src/CompetitionService.BusinessLogic/Contracts/Services/ICompetitionBaseService.cs
@@ -0,0 +1,11 @@
+﻿using CompetitionService.BusinessLogic.Models.Competitions;
+
+namespace CompetitionService.BusinessLogic.Contracts.Services
+{
+    public interface ICompetitionBaseService
+    {
+        Task BlockCompetitionBaseById(Guid id, CancellationToken token);
+
+        Task CompleteCompetitionBaseOutcomes(CompetitionBase competitionBase, CancellationToken token);
+    }
+}
---
+++ src/CompetitionService.BusinessLogic/Extensions/CompetitionBaseBlocker.cs
@@ -0,0 +1,26 @@
+﻿using CompetitionService.BusinessLogic.Enums;
+using CompetitionService.BusinessLogic.Models.Competitions;
+
+namespace CompetitionService.BusinessLogic.Extensions
+{
+    /// <summary>
+    /// Provides functionality for setting coefficients status.
+    /// </summary>
+    public static class CompetitionBaseBlocker
+    {
+        /// <summary>
+        /// Sets the coefficients status.
+        /// </summary>
+        /// <param name="competitionBase">The competition base.</param>
+        /// <param name="type">The type.</param>
+        /// <returns><seealso cref="CompetitionBase"/></returns>
+        public static CompetitionBase SetCoefficientsStatus(this CompetitionBase competitionBase, CoefficientStatusType type)
+        {
+            competitionBase.CoefficientGroups
+                .ForEach(x => x.Coefficients
+                .ForEach(x => x.StatusType = type));
+
+            return competitionBase;
+        }
+    }
+}
