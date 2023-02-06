0e30e35842fc9b66c95a4fe69ccffe75605b861a
nikita.prachkovski <nikita.prahkovski@effective-soft.com>
2023-02-06 11:19:10 +0300

    add helm

--- tests/CompetitionService.IntegrationTests/DataAccess/Repositories/CompetitionDota2RepositoryTests.cs
+++ tests/CompetitionService.IntegrationTests/DataAccess/Repositories/CompetitionDota2RepositoryTests.cs
@@ -33,16 +33,16 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
 
             var competitionDota2 = new CompetitionDota2()
             {
-                Id = Guid.Parse("3248299a-700e-48bf-8601-eb48dc482813"),
-                Team1Id = Guid.Parse("d8705402-b376-4a2b-8959-a8da15e96b9b"),
-                Team2Id = Guid.Parse("a71f00c1-b271-422b-a18a-ae120b1227fd"),
+                Id = Guid.Parse("7a679df5-9d39-45eb-a99b-c53eafc88a0b"),
+                Team1Id = Guid.Parse("1b5735ce-43fb-4b5f-a8e2-0b89383f6593"),
+                Team2Id = Guid.Parse("09d7eb50-901c-4f22-98f7-c9af474ac5b9"),
                 Team1KillAmount = 1,
                 Team2KillAmount = 1,
-                CompetitionBaseId = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                CompetitionBaseId = Guid.Parse("e9f6c341-0137-413d-aa57-ad80c1d2e271"),
                 TotalTime = DateTime.MinValue,
                 CompetitionBase = new CompetitionBase()
                 {
-                    Id = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                    Id = Guid.Parse("b6f43b5a-c037-404e-8cfd-8d39b54d5e77"),
                     StartTime = DateTime.MinValue,
                     StatusType = CompetitionStatusType.Live,
                     Type = CompetitionType.EsportDota2,
@@ -50,15 +50,15 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                     {
                         new CoefficientGroup()
                         {
-                            Id = Guid.Parse("c4c94cc4-5bae-47fe-be17-d8464a71e338"),
-                            CompetitionBaseId = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                            Id = Guid.Parse("dcd66d95-2c8b-4480-9844-4a9e5f0531c8"),
+                            CompetitionBaseId = Guid.Parse("3d32fbb9-0034-4d75-b1d5-5c0a8398c885"),
                             Name = "winner",
                             Type = CoefficientGroupType.OneWinner,
                             Coefficients= new List<Coefficient>()
                             {
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("6c4e5394-ab0a-4c15-b6cc-117b67f74db5"),
+                                    Id = Guid.Parse("f582c094-3b31-46ce-bc8a-a6650cb27c58"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -67,8 +67,8 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                                 },
                                 new Coefficient()
                                 {
-                                    CoefficientGroupId = Guid.Parse("c4c94cc4-5bae-47fe-be17-d8464a71e338"),
-                                    Id = Guid.Parse("10de3338-1995-4b77-9ca5-0ba4e8ff6f66"),
+                                    CoefficientGroupId = Guid.Parse("e27543d8-f40b-4e8e-b04b-aaee17679ff7"),
+                                    Id = Guid.Parse("94e75b14-64f0-4ebc-8032-2a71c838e5c1"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -79,14 +79,14 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                         },
                         new CoefficientGroup()
                         {
-                            Id = Guid.Parse("2f87f471-e7d5-4ee4-b9d7-e37e86ae01e1"),
+                            Id = Guid.Parse("d641a457-5e36-40a2-8ead-41d14090c4c3"),
                             Name = "most kills",
                             Type = CoefficientGroupType.TwoWinners,
                             Coefficients= new List<Coefficient>()
                             {
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("9850b658-c7e8-40fe-958d-b13a8fca3d4c"),
+                                    Id = Guid.Parse("3b5e6bd4-00bf-4283-9a0e-f09c419957c4"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -95,7 +95,7 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                                 },
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("2484afdd-2fe3-4b77-aa02-975ba6687bd5"),
+                                    Id = Guid.Parse("1b0fabcc-0b77-4e85-8840-00185e8276df"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -112,16 +112,16 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
             // Act
             var expectedCompetition = new CompetitionDota2()
             {
-                Id = Guid.Parse("3248299a-700e-48bf-8601-eb48dc482813"),
-                Team1Id = Guid.Parse("d8705402-b376-4a2b-8959-a8da15e96b9b"),
-                Team2Id = Guid.Parse("a71f00c1-b271-422b-a18a-ae120b1227fd"),
+                Id = Guid.Parse("7a679df5-9d39-45eb-a99b-c53eafc88a0b"),
+                Team1Id = Guid.Parse("1b5735ce-43fb-4b5f-a8e2-0b89383f6593"),
+                Team2Id = Guid.Parse("09d7eb50-901c-4f22-98f7-c9af474ac5b9"),
                 Team1KillAmount = 1,
                 Team2KillAmount = 1,
-                CompetitionBaseId = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                CompetitionBaseId = Guid.Parse("e9f6c341-0137-413d-aa57-ad80c1d2e271"),
                 TotalTime = DateTime.MinValue,
                 CompetitionBase = new CompetitionBase()
                 {
-                    Id = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                    Id = Guid.Parse("b6f43b5a-c037-404e-8cfd-8d39b54d5e77"),
                     StartTime = DateTime.MinValue,
                     StatusType = CompetitionStatusType.Live,
                     Type = CompetitionType.EsportDota2,
@@ -129,15 +129,15 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                     {
                         new CoefficientGroup()
                         {
-                            Id = Guid.Parse("c4c94cc4-5bae-47fe-be17-d8464a71e338"),
-                            CompetitionBaseId = Guid.Parse("d4036d01-da0b-4809-b989-f085d517dec7"),
+                            Id = Guid.Parse("dcd66d95-2c8b-4480-9844-4a9e5f0531c8"),
+                            CompetitionBaseId = Guid.Parse("3d32fbb9-0034-4d75-b1d5-5c0a8398c885"),
                             Name = "winner",
                             Type = CoefficientGroupType.OneWinner,
                             Coefficients= new List<Coefficient>()
                             {
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("6c4e5394-ab0a-4c15-b6cc-117b67f74db5"),
+                                    Id = Guid.Parse("f582c094-3b31-46ce-bc8a-a6650cb27c58"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -146,8 +146,8 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                                 },
                                 new Coefficient()
                                 {
-                                    CoefficientGroupId = Guid.Parse("c4c94cc4-5bae-47fe-be17-d8464a71e338"),
-                                    Id = Guid.Parse("10de3338-1995-4b77-9ca5-0ba4e8ff6f66"),
+                                    CoefficientGroupId = Guid.Parse("e27543d8-f40b-4e8e-b04b-aaee17679ff7"),
+                                    Id = Guid.Parse("94e75b14-64f0-4ebc-8032-2a71c838e5c1"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -158,14 +158,14 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                         },
                         new CoefficientGroup()
                         {
-                            Id = Guid.Parse("2f87f471-e7d5-4ee4-b9d7-e37e86ae01e1"),
+                            Id = Guid.Parse("d641a457-5e36-40a2-8ead-41d14090c4c3"),
                             Name = "most kills",
                             Type = CoefficientGroupType.TwoWinners,
                             Coefficients= new List<Coefficient>()
                             {
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("9850b658-c7e8-40fe-958d-b13a8fca3d4c"),
+                                    Id = Guid.Parse("3b5e6bd4-00bf-4283-9a0e-f09c419957c4"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -174,7 +174,7 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
                                 },
                                 new Coefficient()
                                 {
-                                    Id = Guid.Parse("2484afdd-2fe3-4b77-aa02-975ba6687bd5"),
+                                    Id = Guid.Parse("1b0fabcc-0b77-4e85-8840-00185e8276df"),
                                     Description = "desc",
                                     Amount= 0,
                                     Rate = 1,
@@ -191,7 +191,7 @@ namespace CompetitionService.IntegrationTests.DataAccess.Repositories
             await _competitionRepository.Create(competitionDota2, _ct);
             await _context.SaveChanges(_ct);
 
-            var existingCompetitionDota2Id = Guid.Parse("3248299a-700e-48bf-8601-eb48dc482813");
+            var existingCompetitionDota2Id = Guid.Parse("7a679df5-9d39-45eb-a99b-c53eafc88a0b");
             var existingCompetitionDota = await _competitionProvider.GetById(existingCompetitionDota2Id, _ct);
 
             // Assert
