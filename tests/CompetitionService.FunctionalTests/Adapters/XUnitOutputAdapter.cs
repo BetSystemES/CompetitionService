ebf2e323415a167f184ee17bd921ea6a307a9b60
nikita.prachkovski <nikita.prahkovski@effective-soft.com>
2023-02-24 20:17:47 +0300

    implemnt ScenarioCreateCompetitionDota2Tests

---
+++ tests/CompetitionService.FunctionalTests/Adapters/XUnitOutputAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace CompetitionService.FunctionalTests.Adapters
+{
+    public class XUnitOutputAdapter : TextWriter
+    {
+        private readonly ITestOutputHelper _output;
+
+        public XUnitOutputAdapter(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public override void WriteLine(string? value)
+        {
+            _output.WriteLine(value);
+        }
+
+        public override Encoding Encoding { get; } = Encoding.UTF8;
+    }
+}
